208311f4ab4bf3bd76e8272ad169003fc2565da0
HamdanFauzi211 <hamdanfauzi281@gmail.com>
2022-03-05 11:49:25 +0700

    Mengerjakan Modul 3

---
+++ my-app/src/component/HelloComponent.js
@@ -0,0 +1,12 @@
+// Agar function arrow HelloComponent bisa berjalan di react maka tambahan react
+
+import React from 'react';
+import './HelloComponent.css';
+
+const HelloComponent = () =>{
+    return <p className='text-p'> Nama saya adalah Hamdan Daalal Fauzi</p>
+
+}
+
+// agar component ini dapat dipakai dimana saja
+export default HelloComponent;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Test from './Test';
+import HelloComponent from './component/HelloComponent'; 
 
-const Hello = () => {
-  return <p>Hello</p>
+
+
+
+class StateFullComponent extends React.Component {
+  render() {
+    return <p>StateFullComponent</p>
+  }
 }
+
+ReactDOM.render(
+  <HelloComponent />,
+  document.getElementById('root')
+);
 
 // ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>, document.getElementById('root'));
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
